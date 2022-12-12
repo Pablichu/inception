5292f179063c1758e726f0d167deb5703624f5e6
pablo <pablo200016@gmail.com>
2022-12-12 21:22:14 +0100

    mariadb script developing

--- srcs/requirements/mariadb/conf/setup.sh
+++ srcs/requirements/mariadb/conf/setup.sh
@@ -1,53 +1,39 @@
-# The version of MariaDB in the Alpine repositories behave like
-# the MySQL tarball. No graphical tools are included.
-# 
-# The datadir located at /var/lib/mysql must be owned by the mysql
-# user and group.
-# 
-# Normal initialization of mariadb can be done as follows:
-# 
-#     -Initialize the main mysql database, and the data dir
-# 	    as standardized to /var/lib/mysql by running /etc/init.d/mariadb setup
-#     -Start the main service. At this point there will be
-# 	    no root password set. /etc/init.d/mariadb start
-#     -Secure the database by running mysql_secure_installation
-#     -Setup permissions for managing others users and databases
-# 
-# Configures mysql with a file in "/etc/my.cnf".
-
 #!/bin/sh
-
-chown mysql:mysql -R /var/lib/mysql
-
-/etc/init.d/mariadb start
-/usr/bin/mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
-
-# Automate automate-mysql_secure_installation
-# Make sure that NOBODY can access the server without a password
-# Kill the anonymous users
-# Because our hostname varies we'll use some Bash magic here.
-# Kill off the demo database
-# Make our changes take effect
-# Any subsequent tries to run queries this way will get access denied because lack of usr/pwd param
-readonly MYSQL_SECURE="UPDATE mysql.user SET Password = PASSWORD($MYSQL_ROOT_PASSWORD) WHERE User = 'root';
-DROP USER ''@'localhost';
-DROP USER ''@'$(hostname)';
-DROP DATABASE test;
-FLUSH PRIVILEGES;"
-
 #Create wordpress database
 readonly DB_CREATE="CREATE DATABASE $WP_DB_NAME;
-CREATE USER $MYSQL_USER@localhost IDENTIFIED BY $MYSQL_PASSWORD;
+CREATE USER $MYSQL_USER@localhost IDENTIFIED BY $MYSQL_PW;
 GRANT ALL PRIVILEGES ON $WP_DB_NAME.* TO $MYSQL_USER@localhost ;
 FLUSH PRIVILEGES;
 EXIT;"
 
-readonly SQL="{MYSQL_SECURE}{DB_CREATE}"
+# Automate automate-mysql_secure_installation
+readonly MYSQL_SECURE="UPDATE mysql.user SET Password = PASSWORD($MYSQL_ROOT_PW) WHERE User = 'root';DROP USER ''@'localhost';DROP USER ''@'$(hostname)';DROP DATABASE test;FLUSH PRIVILEGES;EXIT;"
 
-mysql -u root -p -e "$SQL"
+if [ ! -d "/run/mysql" ]; then
+    mkdir -p "/run/mysql"
+fi
+chown -R mysql:mysql "/run/mysql"
+
+if [ ! -d "var/lib/mysql/$WP_DB_NAME" ]; then
+    if [ ! -d "var/lib/mysql" ]; then
+    echo "Cabroooon"
+        mkdir -p "var/lib/mysql"
+    fi
+    chown -R mysql:mysql "/var/lib/mysql"
+    mysql_install_db --user=mysql --ldata=/var/lib/mysql > /dev/null
+
+    #echo "$DB_CREATE"
+    #echo "$MYSQL_SECURE"
+
+    mysql -u root -p -e "$DB_CREATE"
+    #mysql -u root -p -e "$MYSQL_SECURE"
+else
+    chown mysql:mysql -R /var/lib/mysqld
+fi
 
 #Allow 
 sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/mysql/my.cnf
 sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
 
-rm /tmp/setup.sh
+#rm /tmp/setup.sh
+exec /usr/bin/mysqld --user=mysql --console --bind-address=0.0.0.0
