eccedeaadd156d6f4b602e377628427f831160bd
pablo <pablo200016@gmail.com>
2022-12-28 13:58:14 +0100

    Mariadb working

--- srcs/requirements/mariadb/conf/setup.sh
+++ srcs/requirements/mariadb/conf/setup.sh
@@ -1,40 +1,42 @@
 #!/bin/sh
-#Create wordpress database
-readonly DB_CREATE="CREATE DATABASE $WP_DB_NAME;
-CREATE USER $MYSQL_USER@localhost IDENTIFIED BY $MYSQL_PW;
-GRANT ALL PRIVILEGES ON $WP_DB_NAME.* TO $MYSQL_USER@localhost ;
-FLUSH PRIVILEGES;
-EXIT;"
-
-# Automate automate-mysql_secure_installation
-readonly MYSQL_SECURE="UPDATE mysql.user SET Password = PASSWORD($MYSQL_ROOT_PW) WHERE User = 'root';DROP USER ''@'localhost';DROP USER ''@'$(hostname)';DROP DATABASE test;FLUSH PRIVILEGES;EXIT;"
 
 if [ ! -d "/run/mysqld" ]; then
     mkdir -p "/run/mysqld"
 fi
-chown -R mysql:mysql "/run/mysqld"
+if [ ! -d "var/lib/mysql" ]; then
+    mkdir -p "var/lib/mysql"
+fi
+chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
 
 if [ ! -d "var/lib/mysql/$WP_DB_NAME" ]; then
-    if [ ! -d "var/lib/mysql" ]; then
-    echo "Cabroooon"
-        mkdir -p "var/lib/mysql"
-    fi
-    chown -R mysql:root "/var/lib/mysql"
     mysql_install_db --user=mysql --ldata=/var/lib/mysql > /dev/null
 
 
-    #echo "$DB_CREATE"
-    #echo "$MYSQL_SECURE"
+	tfile=`mktemp`
+	if [ ! -f "$tfile" ]; then
+		return 1
+	fi
 
-    mysql -u root -p -e "$DB_CREATE"
-    #mysql -u root -p -e "$MYSQL_SECURE"
-else
-    chown mysql:mysql -R /var/lib/mysqld
+#UPDATE mysql.user SET Password = PASSWORD($MYSQL_ROOT_PW) WHERE User = 'root';
+#DROP USER ''@'localhost';
+#DROP USER ''@'$(hostname)';
+#DROP DATABASE test;
+#FLUSH PRIVILEGES;
+	cat << EOF > $tfile
+FLUSH PRIVILEGES;
+CREATE DATABASE IF NOT EXISTS $WP_DB_NAME;
+CREATE USER '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PW';
+GRANT ALL PRIVILEGES ON $WP_DB_NAME.* TO '$MYSQL_USER'@'localhost';
+FLUSH PRIVILEGES;
+EOF
+	# run init.sql
+	/usr/bin/mysqld --user=root --bootstrap < $tfile
+	rm -f $tfile
 fi
 
 #Allow 
-#sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/mysql/my.cnf
+sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf
 sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
 
-exec /usr/bin/mysqld --user=mysql --console  --bind-address=0.0.0.0
+exec /usr/bin/mysqld -u mysql --console
 #tail -f /dev/null
