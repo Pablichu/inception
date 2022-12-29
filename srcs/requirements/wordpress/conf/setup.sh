9c427a9b82cc9e04ea6ee57c28971fa2f2cc83af
pmira-pe <pmira-pe@student.42madrid.com>
2022-12-29 21:01:59 +0100

    adding www user for wordpress

--- srcs/requirements/wordpress/conf/setup.sh
+++ srcs/requirements/wordpress/conf/setup.sh
@@ -2,6 +2,8 @@
 
 if [ ! -f /var/www/html/wordpress/wp-config.php ]; then
 
+adduser -u 82 -D -S -G www-data www-data
+
 wget http://wordpress.org/latest.tar.gz
 tar vxfz latest.tar.gz
 rm -rf latest.tar.gz
@@ -13,6 +15,7 @@ sed -i "s/localhost/localhost:3306/g" wordpress/wp-config-sample.php
 mv wordpress/wp-config-sample.php wordpress/wp-config.php
 
 mv wordpress /var/www/html
+chown www-data:www-data /var/www/html
 
 fi
 
