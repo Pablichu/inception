a545c3eaa8dac52be628870e026e8b95e01da9ca
pmira-pe <pmira-pe@student.42madrid.com>
2023-01-10 21:14:04 +0100

    future automation of redis plugin installation

--- srcs/requirements/wordpress/conf/setup.sh
+++ srcs/requirements/wordpress/conf/setup.sh
@@ -14,7 +14,8 @@ mv wordpress/wp-config-sample.php wordpress/wp-config.php
 
 mv wordpress /var/www/html
 #chown www-data:www-data /var/www/html/wordpress
-
+#wget https://downloads.wordpress.org/plugin/redis-cache.2.2.2.zip
+#mv object-cache.php /var/www/html/wordpress/wp-content/
 fi
 sed -i "s/127.0.0.1:9000/0.0.0.0:9000/g" /etc/php7/php-fpm.d/www.conf
 
