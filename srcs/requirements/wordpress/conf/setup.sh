f6d6dde5f914b16d8f256ba2b655c0deff6fd8a8
pmira-pe <pmira-pe@student.42madrid.com>
2023-01-11 18:01:33 +0100

    Little changes

--- srcs/requirements/wordpress/conf/setup.sh
+++ srcs/requirements/wordpress/conf/setup.sh
@@ -2,6 +2,8 @@
 
 if [ ! -f /var/www/html/wordpress/wp-config.php ]; then
 
+sed -i "s/127.0.0.1:9000/0.0.0.0:9000/g" /etc/php7/php-fpm.d/www.conf
+
 wget http://wordpress.org/latest.tar.gz
 tar vxfz latest.tar.gz
 rm -rf latest.tar.gz
@@ -13,10 +15,7 @@ sed -i "s/localhost/mariadb/g" wordpress/wp-config-sample.php
 mv wordpress/wp-config-sample.php wordpress/wp-config.php
 
 mv wordpress /var/www/html
-#chown www-data:www-data /var/www/html/wordpress
 #wget https://downloads.wordpress.org/plugin/redis-cache.2.2.2.zip
-#mv object-cache.php /var/www/html/wordpress/wp-content/
-fi
-sed -i "s/127.0.0.1:9000/0.0.0.0:9000/g" /etc/php7/php-fpm.d/www.conf
+#mv object-cache.php /var/www/html/wordpress/wp-content/plugin
 
 exec /usr/sbin/php-fpm7 -F
