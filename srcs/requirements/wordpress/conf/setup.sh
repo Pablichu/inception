c7da3c6bf30b1a51870ddfff2e5104833eee3775
pablo <pablo200016@gmail.com>
2023-01-14 15:32:59 +0100

    redis plugin installation automated

--- srcs/requirements/wordpress/conf/setup.sh
+++ srcs/requirements/wordpress/conf/setup.sh
@@ -12,6 +12,7 @@ sed -i "s/database_name_here/$WP_DB_NAME/g" wordpress/wp-config-sample.php
 sed -i "s/username_here/$MYSQL_USER/g" wordpress/wp-config-sample.php
 sed -i "s/password_here/$MYSQL_PW/g" wordpress/wp-config-sample.php
 sed -i "s/localhost/mariadb/g" wordpress/wp-config-sample.php
+dos2unix wordpress/wp-config-sample.php
 mv wordpress/wp-config-sample.php wordpress/wp-config.php
 
 mv wordpress /var/www/html
@@ -24,8 +25,13 @@ if [ ! -f "/var/www/html/wordpress/wp-content/plugins/object-cache.php" ]; then
 wget https://downloads.wordpress.org/plugin/redis-cache.2.2.3.zip
 unzip redis-cache.2.2.3.zip
 rm redis-cache.2.2.3.zip
+
+chown -R nobody:nobody redis-cache
 cp redis-cache/includes/object-cache.php /var/www/html/wordpress/wp-content/
 mv redis-cache /var/www/html/wordpress/wp-content/plugins
+
+sed -i "2 a define( 'WP_CACHE_KEY_SALT', 'pmira-pe.42.fr' );" wordpress/wp-config.php
+sed -i "3 a define( 'WP_REDIS_HOST', 'redis' );" wordpress/wp-config.php
 
 fi
 
