8eed7633a633c28ede5d25eb4e0f4c129c1a692d
pmira-pe <pmira-pe@student.42madrid.com>
2023-02-09 17:22:22 +0100

    fixing sed conf

--- srcs/requirements/bonus/ftp/conf/setup.sh
+++ srcs/requirements/bonus/ftp/conf/setup.sh
@@ -1,13 +1,13 @@
 sed -i "/^#/d" /etc/vsftpd/vsftpd.conf && \
-sed -i "seccomp_sandbox=NO" /etc/vsftpd/vsftpd.conf && \
-sed -i "local_enable=NO" /etc/vsftpd/vsftpd.conf && \
-sed -i "anon_upload_enable=YES" /etc/vsftpd/vsftpd.conf && \
-sed -i "write_enable=YES" /etc/vsftpd/vsftpd.conf && \
-sed -i "anon_mkdir_write_enable=YES" /etc/vsftpd/vsftpd.conf && \
-sed -i "anon_root=/var/www/html" /etc/vsftpd/vsftpd.conf && \
-sed -i "pasv_enable=YES" /etc/vsftpd/vsftpd.conf && \
-sed -i "pasv_max_port=21110" /etc/vsftpd/vsftpd.conf && \
-sed -i "pasv_min_port=21100" /etc/vsftpd/vsftpd.conf
+sed -i "3 a define seccomp_sandbox=NO" /etc/vsftpd/vsftpd.conf && \
+sed -i "4 a define local_enable=NO" /etc/vsftpd/vsftpd.conf && \
+sed -i "5 a define anon_upload_enable=YES" /etc/vsftpd/vsftpd.conf && \
+sed -i "6 a define write_enable=YES" /etc/vsftpd/vsftpd.conf && \
+sed -i "7 a define anon_mkdir_write_enable=YES" /etc/vsftpd/vsftpd.conf && \
+sed -i "8 a define anon_root=/var/www/html" /etc/vsftpd/vsftpd.conf && \
+sed -i "9 a define pasv_enable=YES" /etc/vsftpd/vsftpd.conf && \
+sed -i "10 a define pasv_max_port=21110" /etc/vsftpd/vsftpd.conf && \
+sed -i "11 a define pasv_min_port=21100" /etc/vsftpd/vsftpd.conf
 chown ftp /var/www/html/ && \
 chmod 744 /var/www/html/
 
