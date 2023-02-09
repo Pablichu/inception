88a9056084808e5129c9a7810095e64c47e477cb
pmira-pe <pmira-pe@student.42madrid.com>
2023-02-09 15:39:38 +0100

    now its the time

--- srcs/requirements/bonus/static_web/conf/setup.sh
+++ srcs/requirements/bonus/static_web/conf/setup.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-if [ ! -f "/var/www/html/static" ]; then
-
-mkdir /var/www/html
-
-fi
+#if [ ! -f "/var/www/html/static" ]; then
+#
+#mkdir /var/www/html
+#
+#fi
 
 if [ ! -f "/var/www/html/static/index.html" ]; then
 
+mkdir /var/www/html/static
 mv /tmp/* /var/www/html/static
 
 fi
