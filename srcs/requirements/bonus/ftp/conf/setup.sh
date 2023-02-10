1a09567a0ef522c6b8e73bb3252713c84c08ee06
pmira-pe <pmira-pe@student.42madrid.com>
2023-02-10 22:58:42 +0100

    Okay, adding user

--- srcs/requirements/bonus/ftp/conf/setup.sh
+++ srcs/requirements/bonus/ftp/conf/setup.sh
@@ -1,18 +1,27 @@
 #!/bin/sh
 
 if ! grep -q -F "pasv_enable=YES" "/etc/vsftpd/vsftpd.conf"; then
+
 sed -i "/^#/d" /etc/vsftpd/vsftpd.conf
+
 echo "seccomp_sandbox=NO" >> /etc/vsftpd/vsftpd.conf
-echo "local_enable=NO" >> /etc/vsftpd/vsftpd.conf
-echo "anon_upload_enable=YES" >> /etc/vsftpd/vsftpd.conf
+echo "local_enable=YES" >> /etc/vsftpd/vsftpd.conf
 echo "write_enable=YES" >> /etc/vsftpd/vsftpd.conf
-echo "anon_mkdir_write_enable=YES" >> /etc/vsftpd/vsftpd.conf
-echo "anon_root=/var/www/html" >> /etc/vsftpd/vsftpd.conf
+
+echo "chroot_local_user=YES" >> /etc/vsftpd/vsftpd.conf
+echo "allow_writeable_chroot=YES" >> /etc/vsftpd/vsftpd.conf
+echo "local_root=/var/www/html" >> /etc/vsftpd/vsftpd.conf
+
+echo "ftpd_banner=AAAAAAAAAAAH" >> /etc/vsftpd/vsftpd.conf
+
 echo "pasv_enable=YES" >> /etc/vsftpd/vsftpd.conf
 echo "pasv_max_port=21110" >> /etc/vsftpd/vsftpd.conf
 echo "pasv_min_port=21100" >> /etc/vsftpd/vsftpd.conf
-#chown ftp /var/www/html
+
+adduser $FTP_USER --disable-password
+chown $FTP_USER /var/www/html
 #chmod 744 /var/www/html
+
 fi
 
 exec /sbin/tini -- /usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
