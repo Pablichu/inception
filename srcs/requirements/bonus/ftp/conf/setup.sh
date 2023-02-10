c8e88e0ce67cb28a96e4380bfb6962c603590cc5
pmira-pe <pmira-pe@student.42madrid.com>
2023-02-10 21:38:39 +0100

    Now we check if confs are already written

--- srcs/requirements/bonus/ftp/conf/setup.sh
+++ srcs/requirements/bonus/ftp/conf/setup.sh
@@ -1,3 +1,6 @@
+|#!/bin/sh
+
+if [ ! grep -q "pasv_enable=YES" /etc/vsftpd/vsftpd.conf ]; then
 sed -i "/^#/d" /etc/vsftpd/vsftpd.conf
 echo "seccomp_sandbox=NO" >> /etc/vsftpd/vsftpd.conf
 echo "local_enable=NO" >> /etc/vsftpd/vsftpd.conf
@@ -8,7 +11,8 @@ echo "anon_root=/var/www/html" >> /etc/vsftpd/vsftpd.conf
 echo "pasv_enable=YES" >> /etc/vsftpd/vsftpd.conf
 echo "pasv_max_port=21110" >> /etc/vsftpd/vsftpd.conf
 echo "pasv_min_port=21100" >> /etc/vsftpd/vsftpd.conf
-chown ftp /var/www/html
-chmod 744 /var/www/html
+#chown ftp /var/www/html
+#chmod 744 /var/www/html
+fi
 
 exec /sbin/tini -- /usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
