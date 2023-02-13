41264eba40d866fd3605b639aaf927e4eb1affec
pmira-pe <pmira-pe@student.42madrid.com>
2023-02-13 18:38:57 +0100

    Adding imgs to static web

--- srcs/requirements/bonus/static_web/conf/setup.sh
+++ srcs/requirements/bonus/static_web/conf/setup.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
-#if [ ! -f "/var/www/html/static" ]; then
-#
-#mkdir /var/www/html
-#
-#fi
-
 if [ ! -f "/var/www/html/static/index.html" ]; then
 
 mkdir /var/www/html/static
 mv /tmp/* /var/www/html/static
+
+elif [ -f "/tmp/index.html" ]; then
+
+rm -rf /var/www/html/static
+mv /tmp/* /var/www/html/static/*
 
 fi
 
