ae7d3dda794a15100d9d9fe1649b05792ff6f8a7
pmira-pe <pmira-pe@student.42madrid.com>
2023-02-14 18:09:13 +0100

    Root has now a password in mariadb

--- srcs/requirements/mariadb/conf/setup.sh
+++ srcs/requirements/mariadb/conf/setup.sh
@@ -22,6 +22,7 @@ FLUSH PRIVILEGES;
 CREATE DATABASE IF NOT EXISTS $WP_DB_NAME;
 CREATE USER '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PW';
 GRANT ALL ON $WP_DB_NAME.* to '$MYSQL_USER'@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PW';
 FLUSH PRIVILEGES;
 EOF
 	/usr/bin/mysqld --user=root --bootstrap < $tfile
@@ -32,4 +33,4 @@ fi
 sed -i "s/.*bind-address\s*=.*/bind-address=0.0.0.0/g" /etc/my.cnf.d/mariadb-server.cnf
 sed -i "s/skip-networking/#skip-networking/g" /etc/my.cnf.d/mariadb-server.cnf
 
-exec /usr/bin/mysqld -u root 
+exec /usr/bin/mysqld -u root -p $MYSQL_ROOT_PW
